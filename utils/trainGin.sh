89eb0eaef091f7edf80ce1ae35ffedd5d8c7930b
Edward Schwarz <black-ejs@github.com>
2022-03-30 13:33:43 -0400

    tested

--- utils/trainGin.sh
+++ utils/trainGin.sh
@@ -8,25 +8,28 @@ echo RLGIN TRAINING session started at "${TIMESTAMP}"
 MODEL_NICKNAME=${PWD##*/}
 RUN_DIR=rlgin
 
+echo changing directories to "${RUN_DIR}"....
+cd "${RUN_DIR}"
+echo "current working directory = {$PWD}"
+
 echo locating latest-generation weights....
-INPUT_WEIGHTS=`ls $RUN_DIR/weights/weights.h5.* | grep '[0-9]$' | tail -1`
-INPUT_GENERATION_NUMBER=${INPUT_WEIGHTS##*.}
+INPUT_GENERATION_NUMBER=`ls weights/weights.h5.* | grep "[0-9]$" | cut -d. -f3 | sort -n | tail -1`
+INPUT_WEIGHTS="weights/weights.h5.${INPUT_GENERATION_NUMBER}"
 OUTPUT_GENERATION_NUMBER=$((INPUT_GENERATION_NUMBER+1))
 OUTPUT_WEIGHTS="weights/weights.h5.${OUTPUT_GENERATION_NUMBER}"
 LOGFILE="logs/learning_${MODEL_NICKNAME}_${TIMESTAMP}"
 
+echo "-----------------------------"
 echo MODEL_NICKNAME=${MODEL_NICKNAME}
 echo INPUT_WEIGHTS=${INPUT_WEIGHTS}
 echo INPUT_GENERATION_NUMBER=${INPUT_GENERATION_NUMBER}
 echo OUTPUT_GENERATION_NUMBER=${OUTPUT_GENERATION_NUMBER}
 echo OUTPUT_WEIGHTS=${OUTPUT_WEIGHTS}
 echo LOGFILE=${LOGFILE}
+echo "-----------------------------"
 
-echo copying weights....
-cp "${INPUT_WEIGHTS}" "${RUN_DIR}"/weights/weights.h5
-echo changing directories to "${RUN_DIR}"....
-cd "${RUN_DIR}"
-echo "current working directory = {$PWD}"
+echo copying input weights....
+cp "${INPUT_WEIGHTS}" weights/weights.h5
 
 echo updating code base....
 git pull
@@ -36,11 +39,14 @@ echo cleaning up fram any previous runs...
 [ -e "weights/weights.h5.post_training" ] && rm weights/weights.h5.post_training
 echo launching learning process...
 python learningGin.py > "${LOGFILE}"
-echo learning process, capturing weights...
+echo learning process completed
+
+echo capturing post-training weights...
 cp weights/weights.h5.post_training "${OUTPUT_WEIGHTS}"
+
+echo comparing output and input weights
+cmp "${INPUT_WEIGHTS}" "${OUTPUT_WEIGHTS}"
 
 cd ..
 echo '#####################################################'
 
-echo comparing output and input weights
-cmp "${INPUT_WEIGHTS}" "${OUTPUT_WEIGHTS}"
