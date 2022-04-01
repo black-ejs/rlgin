b177fcd2c32085bbd73af40aa090efab015536e4
Edward Schwarz <black-ejs@github.com>
2022-04-01 12:17:55 -0400

    add params for rewards, re-arrange

--- ginDQNParameters.py
+++ ginDQNParameters.py
@@ -18,7 +18,7 @@ def define_parameters():
     #  
     #  the DQN can have any number of layers between the input and output
     #  as specified in the 'layer_sizes' array
-    params['episodes'] = 5000             # gin hands
+    params['episodes'] = 5             # gin hands
     params['layer_sizes'] = [100, 800, 20]   # 'hidden'/interior layers
     params['learning_rate'] = 0.00332519     # snake_ga = 0.00013629
     params['epsilon_decay_linear'] = .0256 # while learning, how quickly randomness decreases in each hand
@@ -28,21 +28,29 @@ def define_parameters():
     params['batch_size'] = 2500
 
     # ##########################################
-    # App/execution Settings
-    timestamp = str(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
-    params['timestamp'] = timestamp
-    params['player_one_name'] = "Primo"    # opponent
-    params['player_two_name'] = "Tempo"    # DQN
-    params['max_steps_per_hand'] = 1000    # turns
-    params['extend_hands'] = True          # recycle duscards when deck is exhausted, up to max_steps_per_hand
+    # gin model control
+    params['win_reward'] = 100 # 0.999          
+    params['loss_reward'] = -10  # -0.001        
+    params['no_winner_reward'] = -5  # -0.0001
+    params['extend_hands'] = True          # recycle discards when deck is exhausted, up to max_steps_per_hand
+    params['max_steps_per_hand'] = 1000    # max turns before no_winner, if 'extend_hands' == TRUE
     params['brandiac_random_percent'] = 90 # assuming opponent = BrandiacGinStrategy 
 
+    # ##########################################
+    # App/execution Settings
+    params['player_one_name'] = "Primo"    # opponent
+    params['player_two_name'] = "Tempo"    # DQN
+
     params['train'] = True
-    params["load_weights"] = True   # False if starting from scratch, True if re-training 
     params["test"] = True           # run from saved weights after training or re-training
-    params["display"] = True
-    params['plot_score'] = False
-    params['log_path'] = 'logs/learningGin_log.' + timestamp +'.txt'
+    params["load_weights"] = True   # False if starting from scratch, True if re-training 
     params['weights_path'] = 'weights/weights.h5'
+
+    # logging 
+    timestamp = str(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+    params['timestamp'] = timestamp
+    params['log_path'] = 'logs/learningGin_log.' + timestamp +'.txt'
+    params["display"] = True
+    params["log_decisions"] = True
 
     return params
