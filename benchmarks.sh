be9d6232075f0f1f156942f65ba420aadb29fe7c
Edward Schwarz <black-ejs@github.com>
2022-04-24 17:28:18 -0400

    util to benchmark perfomance of non-DQN startegies

---
+++ benchmarks.sh
@@ -0,0 +1,10 @@
+##!/bin/bash
+HANDS_TO_PLAY=100
+for PLAYER1 in br90 r d b br br50 br10 
+do 
+    for PLAYER2 in br90 r d b br br50 br10 
+    do 
+        python playGin.py ${HANDS_TO_PLAY} ${PLAYER1} ${PLAYER1}_1 ${PLAYER2} ${PLAYER2}_2 > ${PLAYER1}_vs_${PLAYER2}_${HANDS_TO_PLAY}.log
+    done
+done
+
