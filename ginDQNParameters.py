c64169ca52da07464cf9654d5d188e4878496fb9
Edward Schwarz <black-ejs@github.com>
2022-05-27 15:32:32 -0400

    clean up old impl

--- ginDQNParameters.py
+++ ginDQNParameters.py
@@ -9,7 +9,6 @@ def define_parameters():
     params['player1'] = {}
     params['player1']['name'] = 'Primo'
     #params['player1']['strategy'] = 'br90'
-    #params['player1']['brandiac_random_percent'] = 90 # assuming opponent = BrandiacGinStrategy 
     params['player1']['strategy'] = 'nn-convb'
     params['player1']['nn'] = {}
     params['player1']['nn']['layer_sizes'] = [88, 300, 20]   # 'hidden'/interior layers - per bayesDqn.py April 2022
@@ -59,34 +58,6 @@ def define_parameters():
     params["display"] = True
     params["log_decisions"] = True
 
-
-    ############# these prams for 4-card game, from-scratch learning
-    #params['gamma'] = 0.99                  # value of future rewards
-    #params['noise_epsilon'] = 1/200         # randomness in actions when not training
-    #params['memory_size'] = 25000
-    #params['batch_size'] = 2500
-    #params['layer_sizes'] = [100, 50, 20]  # 'hidden'/interior layers - per bayesDqn.py April 2022
-    #params['epsilon_decay_linear'] = 0.1    # while learning, how quickly randomness decreases in each hand - per bayesDqn.py April 2022
-    ## this is quite high             ^^^^   but perhaps for initial training on the simpolified game is it OK
-    #params['learning_rate'] = 0.01          # how aggressively to modify weights - per bayesDqn.py April 2022
-
-    # ##########################################
-    # gin model control
-    ############# these prams for 4-card game, from-scratch learning
-    #params['win_reward'] = 0.99            # per bayesReward.py April 2022
-    #params['loss_reward'] = -0.0045        # per bayesReward.py April 2022
-    #params['no_winner_reward'] = -0.0001   # per bayesReward.py April 2022
-    #params['brandiac_random_percent'] = 90 # assuming opponent = BrandiacGinStrategy 
-
-    # ##########################################
-    # App/execution Settings
-    #params['player_one_name'] = "Primo"    # opponent
-    #params['player_two_name'] = "Tempo"    # DQN
-
-    #params['train'] = True
-    #params["test"] = True           # run from saved weights after training or re-training
-    #params["load_weights"] = False   # False if starting from scratch, True if re-training 
-    #params['weights_path'] = 'weights/weights.h5'
 
 
     return params
