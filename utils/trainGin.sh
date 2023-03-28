502c49a661c535d3c301984cdceaae1729b5d119
Edward Schwarz <black-ejs@github.com>
2023-03-28 09:21:45 -0400

    PROCESS_WEIGHTS_PATH, cleanup

--- utils/trainGin.sh
+++ utils/trainGin.sh
@@ -1,5 +1,6 @@
 ##!/usr/bin/bash
 ## run a test cycle, this script is usually invoked with "nohup" or the equivalent
+## place your desired weights into "${RUN_DIR}/${PROCESS_WEIGHTS_PATH}.0
 
 TIMESTAMP=`date -u +%Y-%m-%d_%H-%M-%S`
 echo '#####################################################'
@@ -7,17 +8,18 @@ echo RLGIN TRAINING session started at "${TIMESTAMP}"
 
 MODEL_NICKNAME=${PWD##*/}
 RUN_DIR=rlgin
+PROCESS_WEIGHTS_PATH=weights/weights.h5
 
 echo changing directories to "${RUN_DIR}"....
 cd "${RUN_DIR}"
 echo "current working directory = {$PWD}"
 
 echo locating latest-generation weights....
-INPUT_GENERATION_NUMBER=`ls weights/weights.h5.* | grep "[0-9]$" | cut -d. -f3 | sort -n | tail -1`
-INPUT_WEIGHTS="weights/weights.h5.${INPUT_GENERATION_NUMBER}"
+INPUT_GENERATION_NUMBER=`ls ${PROCESS_WEIGHTS_PATH}.* | grep "[0-9]$" | cut -d. -f3 | sort -n | tail -1`
+INPUT_WEIGHTS="${PROCESS_WEIGHTS_PATH}.${INPUT_GENERATION_NUMBER}"
 OUTPUT_GENERATION_NUMBER=$((INPUT_GENERATION_NUMBER+1))
-OUTPUT_WEIGHTS="weights/weights.h5.${OUTPUT_GENERATION_NUMBER}"
-NAME_SCENARIO="learningGin_${MODEL_NICKNAME}.${OUTPUT_GENERATION_NUMBER}"
+OUTPUT_WEIGHTS="${PROCESS_WEIGHTS_PATH}.${OUTPUT_GENERATION_NUMBER}"
+NAME_SCENARIO="trainGin_${MODEL_NICKNAME}.${OUTPUT_GENERATION_NUMBER}"
 LOGFILE="logs/${NAME_SCENARIO}.${TIMESTAMP}.log"
 
 echo "-----------------------------"
@@ -31,20 +33,21 @@ echo LOGFILE=${LOGFILE}
 echo "-----------------------------"
 
 echo copying input weights....
-cp "${INPUT_WEIGHTS}" weights/weights.h5
+cp "${INPUT_WEIGHTS}" "${PROCESS_WEIGHTS_PATH}"
 
 echo updating code base....
 git pull
 echo getting "${MODEL_NICKNAME}"-specific params....
 cp ../ginDQNParameters.py."${MODEL_NICKNAME}" ginDQNParameters.py
 echo cleaning up fram any previous runs...
-[ -e "weights/weights.h5.post_training" ] && rm weights/weights.h5.post_training
-echo launching learning process...
+[ -e "${PROCESS_WEIGHTS_PATH}.post_training" ] && rm ${PROCESS_WEIGHTS_PATH}.post_training
+
+echo launching training process at `date -u +%Y-%m-%d_%H-%M-%S` ...
 python learningGin.py --name_scenario "${NAME_SCENARIO}" --logfile "${LOGFILE}" --generation "${OUTPUT_GENERATION_NUMBER}"
-echo learning process completed
+echo training process completed at `date -u +%Y-%m-%d_%H-%M-%S`
 
 echo capturing post-training weights...
-cp weights/weights.h5.post_training "${OUTPUT_WEIGHTS}"
+cp ${PROCESS_WEIGHTS_PATH}.post_training "${OUTPUT_WEIGHTS}"
 
 echo comparing output and input weights
 if cmp "${INPUT_WEIGHTS}" "${OUTPUT_WEIGHTS}"; then echo "*** WARNING: NO DIFFERENCE BETWEEN ${INPUT_WEIGHTS} and ${OUTPUT_WEIGHTS}"; fi
