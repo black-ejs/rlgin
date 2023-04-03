8321e6c9ad7c26ba1226623f54af831aacdec833
Edward Schwarz <black-ejs@github.com>
2023-04-03 14:48:07 -0400

    wq2

--- utils/trainGin/setup.trainGin.sh
+++ utils/trainGin/setup.trainGin.sh
@@ -6,9 +6,9 @@ git clone https://github.com/black-ejs/rlgin
 mkdir rlgin/logs
 mkdir rlgin/weights
 cp rlgin/ginDQNParameters.py ginDQNParameters.py.${MODEL_NICKNAME}
-cp rlgin/utils/trainGin.sh .
+cp rlgin/utils/trainGin/trainGin.sh .
 chmod a+x ./trainGin.sh
-cp rlgin/utils/trainDriver.sh .
+cp rlgin/utils/trainGin/trainDriver.sh .
 chmod a+x ./trainDriver.sh
 
 echo *******************************************************************
