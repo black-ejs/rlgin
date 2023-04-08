779a182d8825b2f94c72f13e8f980efff62393bd
Edward Schwarz <black-ejs@github.com>
2023-04-08 16:30:09 -0400

    tested

--- utils/series/series_remote_bootstrap_NWO.sh
+++ utils/series/series_remote_bootstrap_NWO.sh
@@ -4,18 +4,24 @@
 REMOTE_HOSTS="r-learn-c-east1b-vm-1 r-learn-c-east1b-vm-2"
 SERIES_NICKNAME="NWO"
 SERIES_INDEXES="1 2 3 4"
-# SERIES_xS_SPEC=~/Documents/dev/projects/${SERIES_NICKNAME}/ginDQNParameters.py."${SERIES_NICKNAME}"
-REMOTE_ID="edward_schwarz" 
-PARAMS_SOURCE_MODEL_NICKNAME=HOT.3.5
-PARAMS_SOURCE_VM=r-learn-b-cent1a-vm-1
-PARAMS_FILE=ginDQNParameters.py."${PARAMS_SOURCE_MODEL_NICKNAME}"
-PARAMS_TARGET_PATH="~/dev/projects/training_ground/${PARAMS_SOURCE_MODEL_NICKNAME}"
-PARAMS_RP="${REMOTE_ID}"@"${PARAMS_SOURCE_VM}"
-SERIES_PARAMS_SPEC=${PARAMS_RP}:"${PARAMS_TARGET_PATH}"/"${PARAMS_FILE}"
 
+#    CHOOSE setup for remote params 
+#REMOTE_ID=edward_schwarz
+#PARAMS_SOURCE_MODEL_NICKNAME=HOT.3.5
+#PARAMS_SOURCE_VM=r-learn-b-cent1a-vm-1
+#PARAMS_FILE=ginDQNParameters.py."${PARAMS_SOURCE_MODEL_NICKNAME}"
+#PARAMS_TARGET_PATH="~/dev/projects/training_ground/${PARAMS_SOURCE_MODEL_NICKNAME}"
+#PARAMS_RP="${REMOTE_ID}"@"${PARAMS_SOURCE_VM}"
+#SERIES_PARAMS_SPEC=${PARAMS_RP}:"${PARAMS_TARGET_PATH}"/"${PARAMS_FILE}"
+# OR CHOOSE setup for local params 
+SERIES_PARAMS_SPEC=`pwd`/ginDQNParameters.py."${SERIES_NICKNAME}".scratch
+
+# ################################################################
+# ################################################################
 CURRENT_SCRIPT_NAME="`basename ${0}`"
 CURRENT_SCRIPT_SOURCE_DIR="`dirname ${0}`"
 CURRENT_SCRIPT_ORIGINAL_EXECUTION_DIR=`pwd`
+REMOTE_REPO_URL=https://github.com/black-ejs/rlgin
 
 echo "*********** ${CURRENT_SCRIPT_NAME}: execution at `date` ***********"
 echo REMOTE_HOSTS="${REMOTE_HOSTS}"
@@ -23,7 +29,16 @@ echo SERIES_NICKNAME="${SERIES_NICKNAME}"
 echo SERIES_INDEXES="${SERIES_INDEXES}"
 echo SERIES_PARAMS_SPEC="${SERIES_PARAMS_SPEC}"
 
-echo ${CURRENT_SCRIPT_SOURCE_DIR}/series_remote_bootstrap "${REMOTE_HOSTS}"  "${SERIES_NICKNAME}"  "${SERIES_INDEXES}" "${SERIES_PARAMS_SPEC}"
-${CURRENT_SCRIPT_SOURCE_DIR}/series_remote_bootstrap.sh "${REMOTE_HOSTS}"  "${SERIES_NICKNAME}" "${SERIES_INDEXES}" "${SERIES_PARAMS_SPEC}"
+echo "     ****** ${CURRENT_SCRIPT_NAME}: getting scripts ******"
+rm -rf ./rlgin
+git clone "${REMOTE_REPO_URL}"
+SERIES_SCRIPTS_SOURCE_DIR=rlgin/utils/series
 
+echo "     ****** ${CURRENT_SCRIPT_NAME}: getting bootstrapping ${SERIES_NICKNAME} @ ${REMOTE_HOSTS} ******"
+echo ${SERIES_SCRIPTS_SOURCE_DIR}/series_remote_bootstrap "${REMOTE_HOSTS}"  "${SERIES_NICKNAME}"  "${SERIES_INDEXES}" "${SERIES_PARAMS_SPEC}"
+${SERIES_SCRIPTS_SOURCE_DIR}/series_remote_bootstrap.sh "${REMOTE_HOSTS}"  "${SERIES_NICKNAME}" "${SERIES_INDEXES}" "${SERIES_PARAMS_SPEC}"
 
+echo "     ****** ${CURRENT_SCRIPT_NAME}: cleaning up ******"
+rm -rf rlgin
+
+echo "     ****** ${CURRENT_SCRIPT_NAME}: execution completed at `date` ******"
