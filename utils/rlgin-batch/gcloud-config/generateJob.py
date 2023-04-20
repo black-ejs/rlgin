f94f6d5d6adcf44e00717f5238eb6ec79633ef5f
Edward Schwarz <black-ejs@github.com>
2023-04-20 02:35:27 -0400

    getting there

--- utils/rlgin-batch/gcloud-config/generateJob.py
+++ utils/rlgin-batch/gcloud-config/generateJob.py
@@ -2,17 +2,12 @@ import time
 from google.cloud import batch_v1
 
 
-def create_script_job_with_template(project_id: str, region: str, job_name: str, template_link: str) -> batch_v1.Job:
+def create_script_job_with_template(template_link: str, script: str) -> batch_v1.Job:
     """
     This method shows how to create a sample Batch Job that will run
     a simple command on Cloud Compute instances created using a provided Template.
 
     Args:
-        project_id: project ID or project number of the Cloud project you want to use.
-        region: name of the region you want to use to run the job. Regions that are
-            available for Batch are listed on: https://cloud.google.com/batch/docs/get-started#locations
-        job_name: the name of the job that will be created.
-            It needs to be unique for each project and region pair.
         template_link: a link to an existing Instance Template. Acceptable formats:
             * "projects/{project_id}/global/instanceTemplates/{template_name}"
             * "{template_name}" - if the template is defined in the same project as used to create the Job.
@@ -26,7 +21,7 @@ def create_script_job_with_template(project_id: str, region: str, job_name: str,
     task = batch_v1.TaskSpec()
     runnable = batch_v1.Runnable()
     runnable.script = batch_v1.Runnable.Script()
-    runnable.script.text = "echo Hello world! This is task ${BATCH_TASK_INDEX}. This job has a total of ${BATCH_TASK_COUNT} tasks."
+    runnable.script.text = script
     # You can also run a script from a file. Just remember, that needs to be a script that's
     # already on the VM that will be running the job. Using runnable.script.text and runnable.script.path is mutually
     # exclusive.
@@ -35,8 +30,8 @@ def create_script_job_with_template(project_id: str, region: str, job_name: str,
 
     # We can specify what resources are requested by each task.
     resources = batch_v1.ComputeResource()
-    resources.cpu_milli = 2000  # in milliseconds per cpu-second. This means the task requires 2 whole CPUs.
-    resources.memory_mib = 16
+    resources.cpu_milli = 1000  # in milliseconds per cpu-second. This means the task requires 2 whole CPUs.
+    resources.memory_mib = 4000 
     task.compute_resource = resources
 
     task.max_retry_count = 2
@@ -45,7 +40,7 @@ def create_script_job_with_template(project_id: str, region: str, job_name: str,
     # Tasks are grouped inside a job using TaskGroups.
     # Currently, it's possible to have only one task group.
     group = batch_v1.TaskGroup()
-    group.task_count = 4
+    group.task_count = 1
     group.task_spec = task
 
     # Policies are used to define on what kind of virtual machines the tasks will run on.
@@ -67,7 +62,20 @@ def create_script_job_with_template(project_id: str, region: str, job_name: str,
     return job
 
 
-def create_job_request(job: batch_v1.Job, project_id: str, region: str, job_name: str, template_link: str) -> batch_v1.Job:
+def create_job_request(job: batch_v1.Job, 
+                       project_id: str, 
+                       region: str, 
+                       job_name: str) -> batch_v1.Job:
+    """
+    Args:
+        job: the batch_v1.Job with the tasks, scripts, containters, templates, etc. 
+            ready for submission
+        job_name: the name of the job that will be created.
+            It needs to be unique for each project and region pair.
+        project_id: project ID or project number of the Cloud project you want to use.
+        region: name of the region you want to use to run the job. Regions that are
+            available for Batch are listed on: https://cloud.google.com/batch/docs/get-started#locations
+    """
     create_request = batch_v1.CreateJobRequest()
     create_request.job = job
     create_request.job_id = job_name
@@ -80,25 +88,34 @@ def submit_job_request(create_request: batch_v1.CreateJobRequest) -> batch_v1.Jo
     client = batch_v1.BatchServiceClient()
     return client.create_job(create_request)
 
+def dump(obj,label:str=None):
+    if not label==None:
+        print(label)
+
+    for att in dir(obj):
+        if not "__" in att:
+            print (f"{att}: {getattr(obj,att)}")
+
 ## #############################################
 if __name__ == '__main__':
 
-    job_id = f"rb-job-{time.time()}".replace('.','-')
-    
-    job = create_script_job_with_template(  "rlgin-batch", 
-                                            "us-central1-a", 
-                                            job_id, 
-                                            "rb-a-s50-template-1")
-    for att in dir(job):
-        print (f"att: {getattr(job,att)}")
+    project_id='rlgin-batch'
+    job_name = "rb-job-"+f"{time.time()}".replace('.','-')[-9:]
+    template_name = "rb-a-s50-template-2" 
+    region = "us-central1"
+    with open("/Users/edward/Documents/dev/projects/rlgin/utils/rlgin-batch/gcloud-config/.job-script") as scriptfile:
+        script = scriptfile.read()
+     
+    #job_id = f"projects/{project_id}/locations/{region}/jobs/job01"
+    template_link = f"projects/{project_id}/global/instanceTemplates/{template_name}"
+    job = create_script_job_with_template(template_link, script)
+    dump(job,"-------- job")
 
-    create_request = create_job_request(  job, "rlgin-batch", 
-                                            "us-central1-a", 
-                                            job_id, 
-                                            "rb-a-s50-template-1")
-    for att in dir(create_request):
-        print (f"att: {getattr(create_request,att)}")
+    create_request = create_job_request(job,
+                                        "rlgin-batch",
+                                        "us-central1",
+                                        job_name)
+    dump(create_request,"-------- create_request")
 
-#    submitted = submit_job_request(create_request)
-#    for att in dir(create_request):
-#        print (f"att: {getattr(create_request,att)}")
+    submitted = submit_job_request(create_request)
+    dump(submitted,"-------- submitted")
