4a12aeed0d0fee4df2705b7da6785dd97ac3e806
Edward Schwarz <black-ejs@github.com>
2023-04-27 01:22:13 -0400

    4-by-20

--- utils/rlgin-batch/gcloud-config/generateJob.py
+++ utils/rlgin-batch/gcloud-config/generateJob.py
@@ -113,6 +113,7 @@ def create_standard_job_request(params_path: str,
     #template_name = "rb-a-s50-template-2" 
     project_id = "rlgin-batch-384320"
     template_name = "rb-t-s50-template-4" 
+
     ts=f"{time.time()}".replace('.','-')[-9:]
     job_name = f"rb-job-{params_path.lower()}-{ts}"
 
@@ -154,7 +155,7 @@ if __name__ == '__main__':
     #params_path="CYA"
     #num_tasks = 20
     params_path="rb-test-1"
-    num_tasks = 1
+    num_tasks = 20
     regions=["us-central1"]
 
     env = {"RLGIN_BATCH_HELLO":"hello"}
