1e982b5937beb1965266098fd408eb27264e8e47
Edward Schwarz <black-ejs@github.com>
2023-04-28 17:56:17 -0400

    trainGin new world

--- utils/trainGin/setup.trainGin.sh
+++
@@ -1,9 +0,0 @@
-##!/usr/bin/bash
-
-MODEL_NICKNAME=${PWD##*/}
-
-cp rlgin/utils/trainGin/trainGin.sh .
-chmod a+x ./trainGin.sh
-cp rlgin/utils/trainGin/trainDriver.sh .
-chmod a+x ./trainDriver.sh
-
--- utils/trainGin/trainGin.sh
+++ utils/trainGin/trainGin.sh
@@ -2,25 +2,33 @@
 # run a training cycle, this script is usually invoked with "nohup" or the equivalent
 # place your desired weights into "${RUN_DIR}/${PROCESS_WEIGHTS_PATH}.0
 
+CURRENT_SCRIPT_NAME="`basename ${0}`"
+CURRENT_SCRIPT_SOURCE_DIR="`dirname ${0}`"
+CURRENT_SCRIPT_ORIGINAL_EXECUTION_DIR=`pwd`
+
 TIMESTAMP=`date -u +%Y-%m-%d_%H-%M-%S`
-echo '#####################################################'
+echo "############# ${CURRENT_SCRIPT_NAME} #################"
 echo RLGIN TRAINING session started at "${TIMESTAMP}"
 
 MODEL_NICKNAME=${PWD##*/}
-RUN_DIR=rlgin
-PROCESS_WEIGHTS_PATH=weights/weights.h5
+CONTROL_DIR=${PWD}
+RUN_DIR=${RLGIN_BATCH_LOCAL_REPO}
+PARAMS_LOC=${CONTROL_DIR}/params
+WEIGHTS_DIR=${CONTROL_DIR}/weights
+LOGS_DIR=${CONTROL_DIR}/logs
+PROCESS_WEIGHTS_PATH=${WEIGHTS_DIR}/weights.h5
 
-echo changing directories to "${RUN_DIR}"....
+echo "############# ${CURRENT_SCRIPT_NAME} changing directories to ${RUN_DIR}...."
 cd "${RUN_DIR}"
 echo "current working directory = {$PWD}"
 
-echo locating latest-generation weights....
+echo "############# ${CURRENT_SCRIPT_NAME} locating latest-generation weights...."
 INPUT_GENERATION_NUMBER=`ls ${PROCESS_WEIGHTS_PATH}.* | grep "[0-9]$" | cut -d. -f3 | sort -n | tail -1`
 INPUT_WEIGHTS="${PROCESS_WEIGHTS_PATH}.${INPUT_GENERATION_NUMBER}"
 OUTPUT_GENERATION_NUMBER=$((INPUT_GENERATION_NUMBER+1))
 OUTPUT_WEIGHTS="${PROCESS_WEIGHTS_PATH}.${OUTPUT_GENERATION_NUMBER}"
 NAME_SCENARIO="trainGin_${MODEL_NICKNAME}.${OUTPUT_GENERATION_NUMBER}"
-LOGFILE="logs/${NAME_SCENARIO}.${TIMESTAMP}.log"
+LOGFILE="${LOGS_DIR}/${NAME_SCENARIO}.${TIMESTAMP}.log"
 
 echo "-----------------------------"
 echo MODEL_NICKNAME=${MODEL_NICKNAME}
@@ -32,26 +40,40 @@ echo OUTPUT_WEIGHTS=${OUTPUT_WEIGHTS}
 echo LOGFILE=${LOGFILE}
 echo "-----------------------------"
 
-echo copying input weights....
+echo "############# ${CURRENT_SCRIPT_NAME} copying input weights...."
 cp "${INPUT_WEIGHTS}" "${PROCESS_WEIGHTS_PATH}"
 
-#echo updating code base....
-#git pull
-echo getting "${MODEL_NICKNAME}"-specific params....
-cp ../ginDQNParameters.py."${MODEL_NICKNAME}" ginDQNParameters.py
-echo cleaning up fram any previous runs...
-[ -e "${PROCESS_WEIGHTS_PATH}.post_training" ] && rm ${PROCESS_WEIGHTS_PATH}.post_training
+echo "############# ${CURRENT_SCRIPT_NAME} getting "${MODEL_NICKNAME}"-specific params...."
+if [ ! -d ${RUN_DIR}/params ]
+then
+	mkdir -p ${RUN_DIR}/params
+fi
+PARAMS_SOURCE=${PARAMS_LOC}/ginDQNParameters.py.${MODEL_NICKNAME}
+FIXUP=`echo ${SCRATCH_ID} | awk '{p=$0; gsub("[.]","_",p); print p;}'`
+PARAMS_MODULE=params/ginDQNParameters_${MODEL_NICKNAME}_${FIXUP}
+PARAMS_TARGET=${LOCAL_REPO}/${PARAMS_MODULE}.py
+if [[ -d ${RLGIN_BATCH_LOCAL_REPO} ]]
+then
+	cp ${PARAMS_LOC}/ginDQNParameters.py.${MODEL_NICKNAME} ${PARAMS_TARGET} 
+else
+	echo "#### ${CURRENT_SCRIPT_NAME} ${RLGIN_BATCH_LOCAL_REPO} not fouund, this could be a problem...."
+fi
 
-echo launching training process at `date -u +%Y-%m-%d_%H-%M-%S` ...
-python learningGin.py --name_scenario "${NAME_SCENARIO}" --logfile "${LOGFILE}" --generation "${OUTPUT_GENERATION_NUMBER}"
+echo "############# ${CURRENT_SCRIPT_NAME} launching training process at `date -u +%Y-%m-%d_%H-%M-%S` ..."
+python learningGin.py \
+            --name_scenario "${NAME_SCENARIO}" \
+            --logfile "${LOGFILE}" \
+        	--params_module ${PARAMS_MODULE} \
+            --werightsfile_2 "${PROCESS_WEIGHTS_PATH}" \
+            --generation "${OUTPUT_GENERATION_NUMBER}"
 echo training process completed at `date -u +%Y-%m-%d_%H-%M-%S`
 
-echo capturing post-training weights...
+echo "############# ${CURRENT_SCRIPT_NAME} capturing post-training weights..."
 cp ${PROCESS_WEIGHTS_PATH}.post_training "${OUTPUT_WEIGHTS}"
 
-echo comparing output and input weights
+echo "############# ${CURRENT_SCRIPT_NAME} comparing output and input weights..."
 if cmp "${INPUT_WEIGHTS}" "${OUTPUT_WEIGHTS}"; then echo "*** WARNING: NO DIFFERENCE BETWEEN ${INPUT_WEIGHTS} and ${OUTPUT_WEIGHTS}"; fi
 
-cd ..
-echo '#####################################################'
+cd ${CONTROL_DIR}
+echo '############# ${CURRENT_SCRIPT_NAME} completed at `date -u +%Y-%m-%d %H:%M:%S` ##################'
 
