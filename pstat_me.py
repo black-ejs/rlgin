af9f907468866c373653b0bd632da8ec1835992a
Edward Schwarz <black-ejs@github.com>
2023-05-07 10:20:49 -0400

    turn profiler stats into readable

---
+++ pstat_me.py
@@ -0,0 +1,17 @@
+
+import sys
+import pstats
+from pstats import SortKey
+
+if __name__ == "__main__":
+	print("doo-doo")
+	print(sys.argv)
+	input_file = sys.argv[1]
+	output_file = f"{input_file}.pstat"
+	if len(sys.argv)>2:
+		output_file = sys.argv[2]
+	
+	with open(output_file, 'w') as o: 
+		p = pstats.Stats(input_file, stream=o)
+		p.sort_stats(SortKey.CUMULATIVE).print_stats()
+	
