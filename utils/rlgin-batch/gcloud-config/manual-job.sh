4602ffd1d3aef0b1dd73bbfe71f460c014c819f8
Edward Schwarz <black-ejs@github.com>
2023-06-05 16:04:15 -0400

    run a job "manually" on a new instance

--- utils/rlgin-batch/gcloud-config/manual-job.sh
+++ utils/rlgin-batch/gcloud-config/manual-job.sh
@@ -4,6 +4,8 @@ export BATCH_JOB_ID=rb-job-manual
 export BATCH_JOB_UID=rb-job-manual
 export BATCH_TASK_COUNT=1
 export BATCH_TASK_INDEX=0
+cd ~
 git clone https://github.com/black-ejs/rlgin
 chmod a+x rlgin/utils/rlgin-batch/gcloud-config/.job-script
-nohup rlgin/utils/rlgin-batch/gcloud-config/.job-script
+TIMESTAMP=`date -u +%Y-%m-%d_%H-%M-%S` 
+nohup rlgin/utils/rlgin-batch/gcloud-config/.job-script 2>&1 > mtrain.out.${TIMESTAMP} &
