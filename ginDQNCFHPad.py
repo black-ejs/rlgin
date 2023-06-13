ae98e588aff1fa9c917216edd6d15aad51f78507
Edward Schwarz <black-ejs@github.com>
2023-06-13 18:09:30 -0400

    handle params['conv_layer_kernels']

--- ginDQNCFHPad.py
+++ ginDQNCFHPad.py
@@ -18,13 +18,17 @@ class ginDQNCFHPad(ginDQNConvFHandOut):
         # no stride, padding of kernel-size-1, no groups, no dilation
         my_padding=[ginDQNConvoFloatPlane.CONVO_KERNEL_SIZE[0]-1,
                  ginDQNConvoFloatPlane.CONVO_KERNEL_SIZE[1]-1]
+        if 'conv_layer_kernels'  in self.params:
+            convo_output_channels =  self.params['conv_layer_kernels']
+        else:
+            convo_output_channels =  ginDQNConvoFloatPlane.CONVO_OUTPUT_CHANNELS
         self.convo_output_size = self.calc_conv_output_size(ginDQNConvoFloatPlane.CONVO_INPUT_CHANNELS, 
-                                            ginDQNConvoFloatPlane.CONVO_OUTPUT_CHANNELS, 
+                                            convo_output_channels, 
                                             ginDQNConvoFloatPlane.CONVO_KERNEL_SIZE, 
                                             ginDQNConvoFloatPlane.CONVO_INPUT_CHANNEL_SIZE,
                                             padding=my_padding)
         return nn.Conv2d(ginDQNConvoFloatPlane.CONVO_INPUT_CHANNELS,
-                         ginDQNConvoFloatPlane.CONVO_OUTPUT_CHANNELS,
+                         convo_output_channels,
                          ginDQNConvoFloatPlane.CONVO_KERNEL_SIZE,
                          padding=my_padding)
 
